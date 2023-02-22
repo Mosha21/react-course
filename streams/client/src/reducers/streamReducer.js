c0f8975e4ce985c2fd27411f0a8b64d2d8f20831
Bernardo Moya <bernardo.moya@wirewheel.io>
2023-02-22 12:31:01 -0600

    Feat: Render streams

--- streams/client/src/reducers/streamReducer.js
+++ streams/client/src/reducers/streamReducer.js
@@ -10,7 +10,7 @@ import _ from "lodash";
 const streamReducer = (state = {}, action) => {
   switch (action.type) {
     case FETCH_STREAMS:
-      return { ...state, ..._.mapKeys(state, "id") };
+      return { ...state, ..._.mapKeys(action.payload, "id") };
     case FETCH_STREAM:
       return { ...state, [action.payload.id]: action.payload };
     case CREATE_STREAM:
