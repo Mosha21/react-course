24bc98e1cb646264d6f62d218cef7d3962c688c1
Bernardo Moya <bernardo.moya@wirewheel.io>
2023-03-01 10:02:24 -0600

    Feat: Change Redux for Context

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,5 +1,28 @@
-function App() {
-  return <div className="ui container">App</div>;
+import React from "react";
+import UserCreate from "./UserCreate";
+import { LanguageStore } from "../context/LanguageContext";
+import ColorContext from "../context/ColorContext";
+import LanguageSelector from "./LanguageSelector";
+
+class App extends React.Component {
+  state = { language: "english" };
+
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
+
+  render() {
+    return (
+      <div className="ui container">
+        <LanguageStore value={this.state.language}>
+          <LanguageSelector />
+          <ColorContext.Provider value="primary">
+            <UserCreate />
+          </ColorContext.Provider>
+        </LanguageStore>
+      </div>
+    );
+  }
 }
 
 export default App;
